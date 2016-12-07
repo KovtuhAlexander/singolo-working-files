83e06a804392f0c2b03fd731313e2bd1fba5fab5
KovtuhAlexander <crytek.kovt@gmail.com>
2016-12-07 10:59:42 +0200

    autoprefixer, removed ie7

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ var gulp      	 = require('gulp'),
 gulp.task('sass', function(){
     return gulp.src('src/scss/**/*.scss')
         .pipe(sass())
-        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8'], { cascade: true }))
         .pipe(gulp.dest('src/css'))
         .pipe(browserSync.reload({stream: true}));
 });
